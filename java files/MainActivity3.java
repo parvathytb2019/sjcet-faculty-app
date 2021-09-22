a9ab65015ae401ed97af23827a91e295e1207265
parvathytb2019 <47378860+parvathytb2019@users.noreply.github.com>
2021-09-22 12:35:28 +0530

    Create MainActivity3.java

---
+++ java files/MainActivity3.java
@@ -0,0 +1,24 @@
+package com.example.sjcet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.util.Linkify;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
